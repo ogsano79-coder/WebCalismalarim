505ea6664f253b0b528323c0e530ad8cd0125a81
Yiğit Oğuz <ogsano79@gmail.com>
2026-02-26 11:40:15 +0300

    Add files via upload

---
+++ Sayfa 175/script.js
@@ -0,0 +1,11 @@
+function resim1() {
+    document.getElementById("buyukResim").src = "resim1.jpg";
+}
+
+function resim2() {
+    document.getElementById("buyukResim").src = "resim2.jpg";
+}
+
+function resim3() {
+    document.getElementById("buyukResim").src = "resim3.jpg";
+}
